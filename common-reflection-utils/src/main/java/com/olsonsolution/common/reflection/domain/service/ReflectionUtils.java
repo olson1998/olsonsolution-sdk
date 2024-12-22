d33f9d90df1a6d4c2548ad20735e4cbc85de0e45
olson1998 <aleksander.inglot98@gmail.com>
2024-12-22 21:04:10 +0100

    Reflection utils as static

--- common-reflection-utils/src/main/java/com/olsonsolution/common/reflection/domain/service/ReflectionUtils.java
+++ common-reflection-utils/src/main/java/com/olsonsolution/common/reflection/domain/service/ReflectionUtils.java
@@ -6,15 +6,13 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.*;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.Map.entry;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ReflectionUtils {
-
-    public static final ReflectionUtils SINGLETON = new ReflectionUtils();
 
     public static boolean isAssignableFrom(Type targetType, Type pretenderType) {
         if (targetType instanceof Class<?> targetClass && pretenderType instanceof Class<?> pretenderClass) {
@@ -54,15 +52,29 @@ public final class ReflectionUtils {
     }
 
     public static List<Field> listFields(Class<?> javaClass, boolean includeSuperClasses) {
-        List<Field> fields = new ArrayList<>();
+        Stream.Builder<Field> fields = Stream.builder();
         reflectFields(javaClass, fields, includeSuperClasses);
-        return fields;
+        return fields.build().toList();
     }
 
     public static List<Method> listMethods(Class<?> javaClass, boolean includeSuperClasses) {
-        List<Method> methods = new ArrayList<>();
+        Stream.Builder<Method> methods = Stream.builder();
         reflectMethods(javaClass, methods, includeSuperClasses);
-        return methods;
+        return methods.build().toList();
+    }
+
+    public static List<Method> listSetters(Class<?> javaClass, boolean includeSuperClasses) {
+        return listMethods(javaClass, includeSuperClasses)
+                .stream()
+                .filter(ReflectionUtils::isSetter)
+                .toList();
+    }
+
+    public static List<Method> listGetters(Class<?> javaClass, boolean includeSuperClasses) {
+        return listMethods(javaClass, includeSuperClasses)
+                .stream()
+                .filter(ReflectionUtils::isGetter)
+                .toList();
     }
 
     public static Optional<Field> findField(Class<?> javaClass, String fieldName, boolean includeSuperClass) {
@@ -72,7 +84,10 @@ public final class ReflectionUtils {
                 .findFirst();
     }
 
-    public static Optional<Field> findField(Class<?> javaClass, Type returnedType, String fieldName, boolean includeSuperClasses) {
+    public static Optional<Field> findField(Class<?> javaClass,
+                                            Type returnedType,
+                                            String fieldName,
+                                            boolean includeSuperClasses) {
         return listFields(javaClass, includeSuperClasses)
                 .stream()
                 .filter(field -> fieldName.equals(field.getName()))
@@ -84,8 +99,7 @@ public final class ReflectionUtils {
         String getterName = resolveGetterName(field);
         return listMethods(field.getDeclaringClass(), false)
                 .stream()
-                .filter(method -> getterName.equals(method.getName()) &&
-                        field.getGenericType().equals(method.getGenericReturnType()))
+                .filter(method -> isSearchedGetter(method, getterName, field.getGenericType()))
                 .findFirst();
     }
 
@@ -93,10 +107,7 @@ public final class ReflectionUtils {
         String setterName = resolveSetterName(field);
         return listMethods(field.getDeclaringClass(), false)
                 .stream()
-                .filter(method -> setterName.equals(method.getName()) &&
-                        Void.TYPE.equals(method.getGenericReturnType()) &&
-                        method.getParameterCount() == 1 &&
-                        field.getGenericType().equals(method.getParameters()[0].getParameterizedType()))
+                .filter(method -> isSearchedSetter(method, setterName, field.getGenericType()))
                 .findFirst();
     }
 
@@ -107,31 +118,70 @@ public final class ReflectionUtils {
                 .findFirst();
     }
 
-    public static Map<TypeVariable<?>, Type> mapGenericsBounds(ParameterizedType parameterizedType) {
+    public static Collection<Map.Entry<TypeVariable<?>, Type>> mapGenericsBounds(ParameterizedType parameterizedType) {
         Stream.Builder<Map.Entry<TypeVariable<?>, Type>> genericBounds = Stream.builder();
-        if (parameterizedType.getRawType() instanceof Class<?> rawClass) {
+        collectGenericsFromParamType(parameterizedType, genericBounds);
+        return genericBounds.build().toList();
+    }
+
+    private static void collectGenerics(Type type,
+                                        Stream.Builder<Map.Entry<TypeVariable<?>, Type>> genericBounds) {
+        if (type instanceof Class<?> javaClass) {
+            collectGenericsFromClass(javaClass, genericBounds);
+        } else if (type instanceof ParameterizedType parameterizedType) {
+            collectGenericsFromParamType(parameterizedType, genericBounds);
+        }
+    }
+
+    private static void collectGenericsFromClass(Class<?> javaClass,
+                                                 Stream.Builder<Map.Entry<TypeVariable<?>, Type>> genericBounds) {
+        TypeVariable<? extends Class<?>>[] generics = javaClass.getTypeParameters();
+        Arrays.stream(generics).forEach(generic -> Arrays.stream(generic.getBounds())
+                .forEach(bound -> genericBounds.add(entry(generic, bound))));
+        Type superClass = javaClass.getGenericSuperclass();
+        if (superClass != null && !Object.class.equals(superClass)) {
+            collectGenerics(superClass, genericBounds);
+        }
+        Arrays.stream(javaClass.getGenericInterfaces())
+                .forEach(interfaceType -> collectGenerics(interfaceType, genericBounds));
+    }
+
+    private static void collectGenericsFromParamType(ParameterizedType parameterizedType,
+                                                     Stream.Builder<Map.Entry<TypeVariable<?>, Type>> genericBounds) {
+        if (parameterizedType.getRawType() instanceof Class<?> javaClass) {
+            collectGenericsFromClass(javaClass, genericBounds);
             Type[] actualTypes = parameterizedType.getActualTypeArguments();
-            TypeVariable<? extends Class<?>>[] generics = rawClass.getTypeParameters();
-            for (int i = 0; i < generics.length; i++) {
-                genericBounds.add(entry(generics[i], actualTypes[i]));
+            TypeVariable<? extends Class<?>>[] generics = javaClass.getTypeParameters();
+            if (actualTypes.length == generics.length) {
+                IntStream.range(0, actualTypes.length)
+                        .forEach(i -> genericBounds.add(entry(generics[i], actualTypes[i])));
+            }
+            Type superClass = javaClass.getGenericSuperclass();
+            if (superClass != null && !Object.class.equals(superClass)) {
+                collectGenerics(superClass, genericBounds);
+            }
+            Arrays.stream(javaClass.getGenericInterfaces())
+                    .forEach(interfaceType -> collectGenerics(interfaceType, genericBounds));
+        }
+    }
+
+    private static void reflectFields(Class<?> javaClass, Stream.Builder<Field> fields, boolean includeSuperClass) {
+        if (javaClass != null) {
+            Field[] declaredFields = javaClass.getDeclaredFields();
+            IntStream.range(0, declaredFields.length).forEach(i -> fields.add(declaredFields[i]));
+            if (includeSuperClass && javaClass.getSuperclass() != null) {
+                reflectFields(javaClass.getSuperclass(), fields, includeSuperClass);
             }
         }
-        return genericBounds.build()
-                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static void reflectFields(Class<?> javaClass, List<Field> classFields, boolean includeSuperClass) {
-        classFields.addAll(Arrays.asList(javaClass.getDeclaredFields()));
-        if (includeSuperClass && classFields.stream().noneMatch(field -> field.getDeclaringClass().equals(javaClass.getSuperclass()))) {
-            reflectFields(javaClass.getSuperclass(), classFields, includeSuperClass);
-        }
-    }
-
-    private static void reflectMethods(Class<?> javaClass, List<Method> methods, boolean includeSuperClass) {
-        methods.addAll(Arrays.asList(javaClass.getDeclaredMethods()));
-        if (includeSuperClass &&
-                methods.stream().noneMatch(m -> m.getDeclaringClass().equals(javaClass.getSuperclass()))) {
-            reflectMethods(javaClass.getSuperclass(), methods, includeSuperClass);
+    private static void reflectMethods(Class<?> javaClass, Stream.Builder<Method> methods, boolean includeSuperClass) {
+        if (javaClass != null) {
+            Method[] declaredMethods = javaClass.getDeclaredMethods();
+            IntStream.range(0, declaredMethods.length).forEach(i -> methods.add(declaredMethods[i]));
+            if (includeSuperClass && javaClass.getSuperclass() != null) {
+                reflectMethods(javaClass.getSuperclass(), methods, includeSuperClass);
+            }
         }
     }
 
@@ -154,6 +204,44 @@ public final class ReflectionUtils {
 
     private static String resolveSetterName(Field field) {
         return "set" + StringUtils.capitalize(field.getName());
+    }
+
+    private static boolean isSetter(Method method) {
+        return isPublicNonStaticMethod(method) &&
+                StringUtils.startsWith(method.getName(), "set") &&
+                method.getParameterCount() == 1;
+    }
+
+    private static boolean isSearchedSetter(Method method, String setterName, Type fieldType) {
+        return isPublicNonStaticMethod(method) &&
+                StringUtils.equals(method.getName(), setterName) &&
+                method.getParameterCount() == 1 &&
+                method.getGenericParameterTypes()[0].equals(fieldType);
+    }
+
+    private static boolean isGetter(Method method) {
+        Type returnedType = method.getGenericReturnType();
+        if (Boolean.TYPE.equals(returnedType) && method.getParameterCount() == 0) {
+            return isPublicNonStaticMethod(method) && StringUtils.startsWith(method.getName(), "is");
+        } else if (method.getParameterCount() == 0) {
+            return isPublicNonStaticMethod(method) && StringUtils.startsWith(method.getName(), "get");
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isSearchedGetter(Method method, String getterName, Type fieldType) {
+        Type returnedType = method.getGenericReturnType();
+        if (returnedType.equals(fieldType) && method.getParameterCount() == 0) {
+            return isPublicNonStaticMethod(method) && StringUtils.equals(method.getName(), getterName);
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isPublicNonStaticMethod(Method method) {
+        int modifiers = method.getModifiers();
+        return Modifier.isPublic(modifiers) && !Modifier.isStatic(modifiers);
     }
 
 }
