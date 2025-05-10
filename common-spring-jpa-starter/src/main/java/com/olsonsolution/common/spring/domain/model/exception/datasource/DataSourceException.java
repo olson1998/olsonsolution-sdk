b5e39e8f21769d973de125eea491b706d597f9df
olson1998 <aleksander.inglot98@gmail.com>
2025-05-10 20:51:45 +0200

    Add DataSourceException class for custom datasource errors
    Introduce a RuntimeException subclass to handle datasource-related exceptions. This adds constructors for error messages and optional causes to improve error handling and clarity.

---
+++ common-spring-jpa-starter/src/main/java/com/olsonsolution/common/spring/domain/model/exception/datasource/DataSourceException.java
@@ -0,0 +1,12 @@
+package com.olsonsolution.common.spring.domain.model.exception.datasource;
+
+public class DataSourceException extends RuntimeException {
+
+    public DataSourceException(String message) {
+        super(message);
+    }
+
+    public DataSourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
