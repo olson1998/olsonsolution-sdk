0ed535ee44b8f3f60103e7dee9b0be4b0e30b07f
olson1998 <aleksander.inglot98@gmail.com>
2025-05-10 20:52:01 +0200

    Refactor vendor conversion logic for improved clarity
    Updated `SqlVendorConversionService` to streamline vendor resolution by introducing `extractVendor` for handling singleton extraction. Improved error handling with `DataSourceException` and added null safety using `@NonNull` annotation. These changes enhance maintainability and robustness.

--- common-spring-jpa-starter/src/main/java/com/olsonsolution/common/spring/domain/service/datasource/SqlVendorConversionService.java
+++ common-spring-jpa-starter/src/main/java/com/olsonsolution/common/spring/domain/service/datasource/SqlVendorConversionService.java
@@ -1,13 +1,17 @@
 package com.olsonsolution.common.spring.domain.service.datasource;
 
 import com.olsonsolution.common.data.domain.port.stereotype.sql.SqlVendor;
+import com.olsonsolution.common.spring.domain.model.exception.datasource.DataSourceException;
 import com.olsonsolution.common.spring.domain.port.repository.datasource.SqlVendorConverter;
 import com.olsonsolution.common.spring.domain.port.repository.datasource.SqlVendorResolver;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,16 +20,23 @@ public class SqlVendorConversionService implements SqlVendorConverter {
     private final List<SqlVendorResolver> vendorResolvers;
 
     @Override
-    public SqlVendor convert(String source) {
-        List<SqlVendor> resolvedVendors = vendorResolvers.stream()
+    public SqlVendor convert(@NonNull String source) {
+        return vendorResolvers.stream()
                 .map(resolver -> resolver.resolve(source))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
-                .toList();
-        if(resolvedVendors.size() == 1) {
-            return resolvedVendors.get(0);
-        } else {
-            throw new IllegalArgumentException("Multiple vendors resolved for name: '%s'".formatted(source));
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toUnmodifiableList(),
+                        vendors -> extractVendor(vendors, source)
+                ));
+    }
+
+    private SqlVendor extractVendor(List<SqlVendor> vendors, String vendorName) {
+        try {
+            return CollectionUtils.extractSingleton(vendors);
+        } catch (IllegalArgumentException e) {
+            throw new DataSourceException("Expected to resolve single vendor for name: '%s', but found: %s"
+                    .formatted(vendorName, vendors), e);
         }
     }
 }
