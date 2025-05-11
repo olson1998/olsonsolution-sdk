4a687d353d57a6c2b3e9db534d99541ca1bcb08b
olson1998 <aleksander.inglot98@gmail.com>
2025-05-11 10:03:08 +0200

    Remove Maven compiler properties and improve dependency management.
    This commit removes `<maven.compiler.source>` and `<maven.compiler.target>` properties across multiple modules, aligning with a centralized build configuration. It also adds new dependencies, updates modules, introduces new repositories, and refactors related configurations to enhance maintainability and consistency.

--- common-ssl/src/main/java/com/olsonsolution/common/ssl/domain/model/FileKeyStore.java
+++
@@ -1,9 +0,0 @@
-package com.olsonsolution.common.ssl.domain.model;
-
-import lombok.NonNull;
-
-import java.io.File;
-import java.security.KeyStore;
-
-public record FileKeyStore(@NonNull File file, @NonNull KeyStore keyStore) {
-}
