b2ad8df53700d186a9051a9138415486d1c70537
olson1998 <aleksander.inglot98@gmail.com>
2025-07-13 13:42:03 +0200

    feature: Initi messagebus modules

---
+++ common-spring-jpa-starter/src/main/java/com/olsonsolution/common/spring/domain/port/stereotype/datasource/timestamp/EmbeddedTimestamp.java
@@ -0,0 +1,9 @@
+package com.olsonsolution.common.spring.domain.port.stereotype.datasource.timestamp;
+
+import org.joda.time.MutableDateTime;
+
+public interface EmbeddedTimestamp {
+
+    MutableDateTime toMutableDateTime();
+
+}
